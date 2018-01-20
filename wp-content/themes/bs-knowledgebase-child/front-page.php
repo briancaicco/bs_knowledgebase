fa1b4abd04177f59c82d641bf8ae06078ca47945
Brian Caicco <briancaicco@gmail.com>
2018-01-20 11:06:18 -0500

    category page adjustment

--- wp-content/themes/bs-knowledgebase-child/front-page.php
+++ wp-content/themes/bs-knowledgebase-child/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<div class="container mt-5 pt-5">
+<div class="container mt-5 pt-md-5">
 	<div class="row justify-content-md-center">
-		<div class="col-3 text-center mt-5" id="homepage">
+		<div class="col-12 col-md-3 text-center mt-5" id="homepage">
 			<a href="https://beanstalkdigital.com/">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bs-logo.svg">
 			</a>
@@ -9,11 +9,11 @@
 		</div>
 	</div>
 	<div class="row justify-content-md-center">
-		<div class="col-8 mt-5">
+		<div class="col-12 col-md-8 mt-5 mb-md-5">
 			<?php get_search_form();?>
 		</div>
 	</div>
-	<div class="row mt-md-5">
+	<div class="row mt-3 mt-md-5 mb-5">
 		<?php get_template_part( 'loop-templates/content', 'categories' ); ?>
 	</div>
 </div>
